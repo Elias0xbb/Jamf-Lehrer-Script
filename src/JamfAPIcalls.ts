ef675193e5d64421b7679d66319cf0d2a19506b9
ek <elias.kiene>
2021-08-07 21:03:55 +0200

    added deleteClass function, added interfaces for returned object types.

--- src/JamfAPIcalls.ts
+++ src/JamfAPIcalls.ts
@@ -1,12 +1,58 @@
 
 import {sendRequest} from './httpsRequests';
 
+// Interfaces
+interface ClassArrayObject {
+	uuid: string,
+	name: string,
+	description: string,
+	locationId: number,
+	source: string,
+	image: string,
+	classAsmIdentifier: number,
+	userGroupId: number,
+	studentCount: number,
+	teacherCount: number,
+	deviceGroupId: number,
+	deviceCount: number,
+	passcodeLockGracePeriod: object,
+	wallpaperId: number
+}
+
+interface DetailedClass extends ClassArrayObject {
+	students: UserObject[],
+	teachers: UserObject[]
+}
+
+interface UserObject {
+	id: number,
+	name: string,
+	email: string,
+	username: string,
+	firstName: string,
+	lastName: string,
+	photo: string
+}
+
+interface DetailedUserObject {
+	id: number,
+    locationId: number,
+    status: string,
+    deviceCount: number,
+    email: string,
+    username: string,
+    domain: string,
+    firstName: string,
+    lastName: string,
+    name: string,
+    groupIds: number[]
+}
 
 /*-< getAllClasses() >-------------------------------------------+
 | Requests and returns all classes.                              |
 | If the request fails five times, the program will be crashed.  |
 +---------------------------------------------------------------*/
-async function getAllClasses() {
+async function getAllClasses(): Promise<ClassArrayObject[]> {
 	for(let i = 0; i < 5; ++i) {
 		try {
 			// Send API request for all classes
@@ -30,7 +76,7 @@ async function getAllClasses() {
 | Requests and returns the class specified by the uuid.                                       |
 | Crashs after five unsuccessful tries but ignores "ClassNotFound" error (returns undefined). |
 +--------------------------------------------------------------------------------------------*/
-async function getClass(uuid: string) {
+async function getClass(uuid: string): Promise<DetailedClass> {
 	for(let i = 0; i < 5; ++i) {
 		try {
 			// Send API request for the class
@@ -47,17 +93,39 @@ async function getClass(uuid: string) {
 	process.exit(1)
 }
 
+/*-< deleteClass(uuid) >------------------------------------------+
+| Deletes specified class, crashes after five unsuccessful tries. |
++----------------------------------------------------------------*/
+async function deleteClass(uuid: string): Promise<string> {
+	for(let i = 0; i < 5; ++i) {
+		try {
+			// Check if uuid is defined and send API request to delete the class
+			if(!uuid || uuid == '') throw new Error('Failed to delete Class {UUID missing} [Function: deleteClass].');
+			let response = <{message: string}> await sendRequest(`/classes/${uuid}`, 'DELETE', null);
+
+			// Throw an error if the response is undefined
+			if(!response) throw new Error(`Undefined response [Function call: deleteClass(${uuid})].`);
+			// Return message (expected value: 'ClassDeleted')
+			return response.message;
+		}
+		catch(e) { var err = e }
+	}
+	
+	console.log(`Failed to delete class ${uuid}\nError = ${err}`);
+	process.exit(1)
+}
+
 /*-< getMembersOf(groupID) >---------------------------------------------+
 | Requests and returns all users of the specified group.                 |
 | If no group is specified, a list of all global users will be returned. |
 | Crashes if the request fails five times in a row.                      |
 +-----------------------------------------------------------------------*/
-async function getMembersOf(groupID: string) {
+async function getMembersOf(groupID: string): Promise<DetailedUserObject[]> {
 	for(let i = 0; i < 5; ++i) {
 		try {
 			// Check if groupID is defined and send API request
 			let group = groupID ? `?memberOf=${groupID}` : '';
-			let users = <{users: string}> await sendRequest(`/users${group}`, 'GET', null);
+			let users = <{users: DetailedUserObject[]}> await sendRequest(`/users${group}`, 'GET', null);
 
 			// Throw error if the response or the 'users' property of the response is undefined
 			if(!users) throw new Error(`Undefined response [Function call: getMembersOf(${groupID})].`);
@@ -73,4 +141,4 @@ async function getMembersOf(groupID: string) {
 	process.exit(1)
 }
 
-export { getAllClasses, getClass, getMembersOf }
+export { getAllClasses, getClass, deleteClass, getMembersOf }
