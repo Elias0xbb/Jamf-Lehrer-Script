27c951a2095d9743be4fffc44751ebf55ca122dc
Elias0xbb <starduck004@gmail.com>
2021-08-11 13:16:41 +0200

    fixes and small additions in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 
+// Helper function that returns the config object
 import {getConfig} from './getConfig';
+// Jamf API call functions lib
 import * as jac from './JamfAPIcalls'
 
 
 /*-< main() >--------------------------------------------------------------------+
-| Contains the programs main logic.                                              |
+| Contains the program's main logic.                                             |
 | Requests all classes and relevant groups, deletes classes without a respective |
 | group, adds missing classes and removes / adds users to correct classes.       |
 +-------------------------------------------------------------------------------*/
@@ -14,9 +16,7 @@ async function main(): Promise<number> {
 		let groups = await getValidGroups();
 		let classes = await jac.getAllClasses();
 
-		console.log(groups);
-		console.log('\n\nClasses:\n')
-		console.log(classes)
+		// Return 0 if the execution was successful
 		return 0;
 	}
 	catch(e) {
@@ -33,14 +33,26 @@ async function main(): Promise<number> {
 async function getValidGroups(): Promise<{name: string, id: number}[]> {
 	// Get the config for the class name regex and ignored class description
 	const config = getConfig();
+	if(!config) throw new Error('Config object undefined [Function call: getValidGroups].');
+	if(!config.classUserGroupRegEx || config.classUserGroupRegEx === '') throw new Error(
+		'config.classUserGroupRegEx undefined [Function call: getValidGroups].'
+	);
+	
 	const clsNameRegEx = new RegExp(config.classUserGroupRegEx);
 	const ignoredDescr = config.createdClassDescription;
+	if(!ignoredDescr || ignoredDescr === '') throw new Error(
+		'ignoredDescr undefined [Function call: getValidGroups].'
+	);
 
 	// Request all groups
 	const allGroups = await jac.getAllGroups();
 
 	// Create and return an array of the relevant groups
 	let validGroups = <{ name: string, id: number }[]> [];
+	// Loop over all groups and test names and description
+	// If the name isn't a valid class name or the description is
+	// equal to config.createdClassDescription, the group will not be
+	// added to the validGroups array
 	allGroups.forEach(({name, id, description}) => {
 		if(description != ignoredDescr && clsNameRegEx.test(name)) {
 			validGroups.push({name: name, id: id});
