dd380285c453282f5d4d6d560ffec37d3787dc44
Elias0xbb <starduck004@gmail.com>
2021-08-11 12:49:46 +0200

    added comments to main function and getValidGroups function

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,46 @@
-// Main Program
+
 import {getConfig} from './getConfig';
 import * as jac from './JamfAPIcalls'
 
-async function main() {
+
+/*-< main() >--------------------------------------------------------------------+
+| Contains the programs main logic.                                              |
+| Requests all classes and relevant groups, deletes classes without a respective |
+| group, adds missing classes and removes / adds users to correct classes.       |
++-------------------------------------------------------------------------------*/
+async function main(): Promise<number> {
 	try {
+		// Get all classes and relevant groups
 		let groups = await getValidGroups();
+		let classes = await jac.getAllClasses();
+
+		console.log(groups);
+		console.log('\n\nClasses:\n')
+		console.log(classes)
+		return 0;
 	}
 	catch(e) {
+		// TODO: Error handling
 		console.error(e);
-		process.exit(1)
+		return 1;
 	}
 }
 
+/*-< getValidGroups() >-----------------------------------------------+
+| Requests all groups and returns an array of all relevant groups for |
+| which a class (should) exist(s).                                    |
++--------------------------------------------------------------------*/
 async function getValidGroups(): Promise<{name: string, id: number}[]> {
+	// Get the config for the class name regex and ignored class description
 	const config = getConfig();
 	const clsNameRegEx = new RegExp(config.classUserGroupRegEx);
 	const ignoredDescr = config.createdClassDescription;
 
+	// Request all groups
 	const allGroups = await jac.getAllGroups();
-	let validGroups = <{ name: string, id: number }[]> [];
 
+	// Create and return an array of the relevant groups
+	let validGroups = <{ name: string, id: number }[]> [];
 	allGroups.forEach(({name, id, description}) => {
 		if(description != ignoredDescr && clsNameRegEx.test(name)) {
 			validGroups.push({name: name, id: id});
@@ -28,3 +49,10 @@ async function getValidGroups(): Promise<{name: string, id: number}[]> {
 	
 	return validGroups;
 }
+
+
+// Call main function
+(async _ => {
+	const err = await main();
+	process.exit(err);
+})()
