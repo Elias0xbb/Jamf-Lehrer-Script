34977f70a1b4eb8b7bd3173e7bb7c2b4a29736cb
unknown <sevin.kaindl@gmail.com>
2021-09-01 04:33:35 +0200

    added check for whether coloredConsoleOutputs is true

--- src/helperFunctions.ts
+++ src/helperFunctions.ts
@@ -19,13 +19,12 @@ const config = (_ => {
 
 
 // Change the color of text in the console:
-// TODO: Check if config.coloredConsoleOutputs is true
-const toRed = (msg: string) => `\x1b[31m${msg}\x1b[0m`;
-const toGreen = (msg: string) => `\x1b[32m${msg}\x1b[0m`;
-const toYellow = (msg: string) => `\x1b[33m${msg}\x1b[0m`;
-const toBlue = (msg: string) => `\x1b[34m${msg}\x1b[0m`;
-const toMagenta = (msg: string) => `\x1b[35m${msg}\x1b[0m`;
-const toCyan = (msg: string) => `\x1b[36m${msg}\x1b[0m`;
+const toRed = (msg: string) => config.coloredConsoleOutputs ? `\x1b[31m${msg}\x1b[0m` : msg;
+const toGreen = (msg: string) => config.coloredConsoleOutputs ? `\x1b[32m${msg}\x1b[0m` : msg;
+const toYellow = (msg: string) => config.coloredConsoleOutputs ? `\x1b[33m${msg}\x1b[0m` : msg;
+const toBlue = (msg: string) => config.coloredConsoleOutputs ? `\x1b[34m${msg}\x1b[0m` : msg;
+const toMagenta = (msg: string) => config.coloredConsoleOutputs ? `\x1b[35m${msg}\x1b[0m` : msg;
+const toCyan = (msg: string) => config.coloredConsoleOutputs ? `\x1b[36m${msg}\x1b[0m` : msg;
 
 // An object storing the the class/group name and the classes uuid + group's id
 interface GroupClassPairObject { name: string, groupID: number, classUUID: string }
