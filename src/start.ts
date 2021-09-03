9b4870d333c613867eea238afe11a1b41a5439e1
Elias0xbb <starduck004@gmail.com>
2021-09-03 19:38:16 +0200

    improved parameter system

--- src/start.ts
+++ src/start.ts
@@ -5,23 +5,51 @@ import * as fs from 'fs';
 import {ConfigObject} from './getConfig';
 
 function setup() {
+	// Get parameters
+	const paramsObj: any = (_ => {
+		let pos = -1;
+		const testRegex = /^\{\s*"params"\s*:\s*(true|false)/;
+		process.argv.forEach((arg, idx) => { if(testRegex.test(arg)) pos = idx })
+		if(pos === -1) return null;
+		try {
+			let p = process.argv[pos];
+			return JSON.parse(p);
+		}
+		catch(e) {
+			console.log(`[StartUp] Warning: ${e}`);
+			return null;
+		}
+	})()
+
+	if(!paramsObj) {
+		console.log("[StartUp] Warning: Parameter Object not found!");
+		return 1;
+	}
+	if(!paramsObj.params) return 0;
+	
 	// Get the config object and update all the parameters that are passed
 	const config = <ConfigObject> JSON.parse(
 		fs.readFileSync('../config/scriptConfig.json', { encoding: 'utf8' }));
-	let changedConfig = false;
 
-	if((process.env.authcode ?? '') != '') {
+	// AUTHCODE:
+	if(paramsObj.authcode) {
 		// Convert utf-8 to base64
-		const auth = btoa(unescape(encodeURIComponent(process.env.authcode)));
+		const auth = btoa(unescape(encodeURIComponent(paramsObj.authcode)));
 		config.authorization = auth;
-		changedConfig = true;
 	}
+	if(paramsObj.teacherGroupName) config.teacherGroupName = paramsObj.teacherGroupName;
+	if(paramsObj.teacherGroupID) config.teacherGroupID = paramsObj.teacherGroupID;
+	if(paramsObj.lfg_dirPath) config.logFileConfig.dirPath = paramsObj.lfg_dirPath;
+	if(paramsObj.lfg_autoClear) config.logFileConfig.autoClear = paramsObj.lfg_autoClear;
+	if(paramsObj.lfg_logFileName) config.logFileConfig.logFileName = paramsObj.lfg_logFileName;
+	if(paramsObj.lfg_enableLogFile) config.logFileConfig.enableLogFile = paramsObj.enableLogFile;
+	if(paramsObj.progressBarWidth) config.progressBarWidth = paramsObj.progressBarWidth;
 
-	if(changedConfig) {
-		const newConfig = JSON.stringify(config);
-		console.log('Overwriting the config...');
-		fs.writeFileSync('../config/scriptConfig.json', newConfig);
-	}
+	
+	// Overwrite the config file
+	const newConfig = JSON.stringify(config, null, 4);
+	console.log('[StartUp] Overwriting the config...');
+	fs.writeFileSync('../config/scriptConfig.json', newConfig);
 }
 
 (() => {
